84bcbcd4efdc70f8d6ff8c841b08d5d2961837a5
Michael King <michael.king@uni-konstanz.de>
2018-11-02 11:18:20 +0100

    add 1D free energy surface

--- vmd_util/live_fes.py
+++ vmd_util/live_fes.py
@@ -1,3 +1,10 @@
+"""
+Functions to plot a free energy surface in VMD and connect it with vmd_frame changes.
+
+TODO:
+ - interpolation for 1D surfaces. Currently it only takes the very next point.
+"""
+
 from VMD import evaltcl
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,14 +34,14 @@ def get_header(inp_file):
         if firstline[-1] == 'projection':
             NUM_CV = int((len(firstline) - 1) / 2)
         else:
-            NUM_CV =int((len(firstline) - 3) / 2)
+            NUM_CV = int((len(firstline) - 3) / 2)
         handle_pi = lambda x: float(eval(x.replace('pi', '*pi') if re.match('\dpi', x) else x))
         for i in range(NUM_CV):
-            header.append(CV(name=firstline[i+2],
-                min=handle_pi(fp.readline().split()[-1]),
-                max=handle_pi(fp.readline().split()[-1]),
-                nbin=int((fp.readline()).split()[-1]),
-                periodic=True if str(fp.readline().split()[-1]) == 'true' else False))
+            header.append(CV(name=firstline[i + 2],
+                             min=handle_pi(fp.readline().split()[-1]),
+                             max=handle_pi(fp.readline().split()[-1]),
+                             nbin=int((fp.readline()).split()[-1]),
+                             periodic=True if str(fp.readline().split()[-1]) == 'true' else False))
     return header
 
 
@@ -92,11 +99,60 @@ def load_colvar(inp_file, cv_names):
     return np.genfromtxt(inp_file, usecols=usecols)
 
 
-def plot(header, data, ax=None):
+def plot_1D(header, data, fes_edges=None, ax=None):
     # Type: (list, numpy.ndarray) -> matplotlib.image.AxesImage
     """
 
-    Function to plot a ``fes.dat`` file.
+    Function to plot a 2D ``fes.dat`` file.
+
+    Parameters
+    ----------
+    header : list
+        header of the ``fes.dat`` file
+    data : numpy.ndarray
+        fes.dat
+    fes_edges : List[numpy.ndarray] or None
+        List of edges from the fes. If None they will be generated with the min, max, bins of the header.
+    ax : None or matplotlib.axes._subplots.AxesSubplot
+        ``ax`` object to plot in.
+        If ``None`` an ax object will be created.
+            * Get ``Figure`` with: ``fig = img.get_figure()``
+            * Get ``Axes`` with: ``ax = fig.get_axes()``
+
+    Returns
+    -------
+    img : matplotlib.image.AxesImage
+
+    """
+
+    import matplotlib.pyplot as plt
+    from itertools import chain
+
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.get_figure()
+
+    # create fes_edges if not provided
+    if fes_edges is None:
+        fes_edges = [np.linspace(header[0].min, header[0].max, header[0].nbin)]
+
+    line, = plt.plot(fes_edges[0], data)
+
+    ax.set_xlabel(header[0].name)
+    ax.set_ylabel(r'Energy [$\frac{KJ}{mol}$]')
+
+    ax.set_xlim([header[0].min, header[0].max])
+    ax.set_ylim([data.min(), data.max()])
+
+    return line
+
+
+def plot_2D(header, data, ax=None):
+    # Type: (list, numpy.ndarray) -> matplotlib.image.AxesImage
+    """
+
+    Function to plot a 2D ``fes.dat`` file.
 
     Parameters
     ----------
@@ -134,6 +190,9 @@ def plot(header, data, ax=None):
     ax.set_xlabel(header[0].name)
     ax.set_ylabel(header[1].name)
 
+    ax.set_xlim([header[0].min, header[0].max])
+    ax.set_ylim([header[1].min, header[1].max])
+
     cbar = plt.colorbar(img, ax=ax)
     cbar.set_label(r'Energy [$\frac{KJ}{mol}$]')
 
@@ -149,10 +208,15 @@ def update(t):
     t : int
         frame
     """
-    find = colvar_data[colvar_data[:,0] == t]
+    find = colvar_data[colvar_data[:, 0] == t]
     if find.size == 0:
         return
-    time, x, y = find[0]
+    if colvar_data.shape[1] == 2:
+        time, x = find[0]
+        y = sorted(zip(abs(fes_edges[0] - x), fes_data),
+                   key=lambda x: x[0])[0][1]
+    else:
+        time, x, y = find[0]
     point.set_data([x], [y])
     point.get_figure().canvas.draw()
 
@@ -205,6 +269,7 @@ def run(fes_file='fes.dat', colvar='COLVAR', connect=True):
     connect : bool
         Switch to connect the plot to the current VMD frame.
     """
+
     assert os.path.exists(fes_file), "FES File: {} not found".format(fes_file)
     assert os.path.exists(colvar), "COLVAR file: {} not found".format(colvar)
 
@@ -213,17 +278,23 @@ def run(fes_file='fes.dat', colvar='COLVAR', connect=True):
     current_frame = int(evaltcl('molinfo {} get frame'.format(molid)))
 
     # load files
-    global colvar_data
+    global colvar_data, fes_edges, fes_data
     header, fes_edges, fes_data = load(fes_file)
+    dimensions = len(fes_edges)
     colvar_data = load_colvar(colvar, [cv.name for cv in header])
 
-    # create plot
     global fig, ax
-    fig, ax = plt.subplots()
-    img, cbar = plot(header, fes_data, ax=ax)
-
-    ax.set_xlim([header[0].min, header[0].max])
-    ax.set_ylim([header[1].min, header[1].max])
+    if dimensions == 1:
+        # create plot
+        fig, ax = plt.subplots()
+        line = plot_1D(header, fes_data, fes_edges, ax=ax)
+    elif dimensions == 2:
+        # create plot
+        fig, ax = plt.subplots()
+        img, cbar = plot_2D(header, fes_data, ax=ax)
+    else:
+        raise NotImplementedError("Only 1D and 2D free energy landscapes are implemented." + \
+                                  "\nFound a {}D landscape!".format(dimensions))
 
     # draw
     global point
