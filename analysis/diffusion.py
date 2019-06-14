e5c804015430fb412b4dc0ed42f5d52917bda086
Michael King <michael.king@uni-konstanz.de>
2019-06-14 15:47:06 +0200

    add support for errors

--- analysis/diffusion.py
+++ analysis/diffusion.py
@@ -79,7 +79,8 @@ def calculate_msd(ag, n_min=1, n_max_number_steps=np.inf, dt=1000):
     return time_vec, xmsd, xmsd_var
 
 
-def calculate_diffusion_coefficent(time, msd, return_per_dim=False, return_fit=False, verbose=False):
+def calculate_diffusion_coefficent(time, msd, sigma=None, absolute_sigma=True, return_per_dim=False, return_fit=False,
+                                   verbose=False):
     """
     Use the follwing units: Time [fs] and MSD : [A^2]
 
@@ -89,6 +90,17 @@ def calculate_diffusion_coefficent(time, msd, return_per_dim=False, return_fit=F
         Time [fs] 1-D time vector
     msd : np.ndarray
         MSD [A^2] array. Can be 1-D or 2-D with shape `(n_points,)` or `(n_points, n_dim)`.
+    sigma : np.ndarray or None, optional
+        The error (e.g. standard deviation) in the MSD values.
+        It has to be `None` if no error should be used or
+        an `np.ndarray` with the shape: `msd.shape == absolute_sigma.shape`.
+        If a relative error is used set `absolute_sigma=False`.
+        Default is `None`.
+    absolute_sigma : bool, optional
+        This refers to the option of `scipy.optimize.curve_fit`.
+        Set this to `True` if the error (`sigma`) has the same units as the `msd`.
+        Set this to `False` if its a unitless error (e.g. percentage).
+        Default is `True`.
     return_per_dim : bool, optional
         Returns also the diffusion coefficent in the different directions.
         Default is `False`.
@@ -105,23 +117,29 @@ def calculate_diffusion_coefficent(time, msd, return_per_dim=False, return_fit=F
         Average diffusion coefficent and standard deviation of it.
     (D, Dsd) : tuple[np.darray, np.darray], optional
         Diffusion coefficent in each dimension and standard deviation of them.
-    (popt, pcov) : tuple[np.darray, np.darray], optional
-        Parameter and covariance matrix of the linear fit.
+    (linear, popt, pcov) : tuple[Callable, np.darray, np.darray], optional
+        Linear function, parameter and covariance matrix of the linear fit.
 
     Examples
     --------
     Average diffusion coefficient and standard deviation
     >>> (Dav, Davsd) = calculate_diffusion_coefficent(time, msd, verbose=True)
 
+    Average diffusion coefficient with error (standard deviation of the MSD)
+    >>> (Dav, Davsd) = calculate_diffusion_coefficent(time_vec, msd, sigma=msd_std)
+
     Average diffusion coefficient and standard deviation for sliced data.
     >>> xslice = slice(10, None, None)
-    >>>> (Dav, Davsd) = calculate_diffusion_coefficent(time[xslice], msd[xslice], verbose=True)
+    >>> (Dav, Davsd) = calculate_diffusion_coefficent(time[xslice], msd[xslice])
 
     Average diffusion coefficient and the diffiusion coefficent per dimension
-    >>>> (Dav, Davsd), (D, Dsd) = calculate_diffusion_coefficent(time, msd, return_per_dim=True)
+    >>> (Dav, Davsd), (D, Dsd) = calculate_diffusion_coefficent(time, msd, return_per_dim=True)
 
     Average diffusion coefficient and fitting parameter
-    >>>> (Dav, Davsd), (popt, pcov) = calculate_diffusion_coefficent(time, msd, return_fit=True)
+    >>> (Dav, Davsd), (linear, popt, pcov) = calculate_diffusion_coefficent(time, msd, return_fit=True)
+
+    Average diffusion coefficient with error
+    >>> (Dav, Davsd) = calculate_diffusion_coefficent(time_vec[xslice], xmsd[xslice], sigma=xmsd_std[xslice], verbose=True)
     """
     # convert_units = 1.0e-5, #convert A^2/fs to m^2/sec
     # convert_units = 0.1,    #convert A^2/fs to cm^2/sec
@@ -130,6 +148,8 @@ def calculate_diffusion_coefficent(time, msd, return_per_dim=False, return_fit=F
 
     if msd.ndim == 1:
         msd = np.atleast_2d(msd).T
+        if sigma is not None:
+            sigma = np.atleast_2d(sigma).T
 
     N, ndims = msd.shape
 
@@ -140,11 +160,18 @@ def calculate_diffusion_coefficent(time, msd, return_per_dim=False, return_fit=F
     assert N == time.size, \
         "MSD do not match time.\n time : {}\nMSD : {}".format(time.shape, msd.shape)
 
+    if sigma is not None:
+        print('\nUse error as sigma vor fitting')
+        assert sigma.shape == msd.shape, 'sigma shape does not match msd shape'
+
     popt = np.empty((ndims, 2), dtype=np.float64)
     pcov = np.empty((ndims, 2, 2), dtype=np.float64)
 
     for i in range(ndims):
-        popt[i], pcov[i] = curve_fit(linear, time, msd[:, i])
+        if sigma is None:
+            popt[i], pcov[i] = curve_fit(linear, time, msd[:, i])
+        else:
+            popt[i], pcov[i] = curve_fit(linear, time, msd[:, i], sigma=sigma[:, i], absolute_sigma=absolute_sigma)
 
     if verbose:
         print('\nFitting results')
@@ -166,9 +193,9 @@ def calculate_diffusion_coefficent(time, msd, return_per_dim=False, return_fit=F
 
     if return_fit:
         if return_per_dim:
-            return (Dav, Davsd), (D, Dsd), (popt, pcov)
+            return (Dav, Davsd), (D, Dsd), (linear, popt, pcov)
         else:
-            return (Dav, Davsd), (popt, pcov)
+            return (Dav, Davsd), (linear, popt, pcov)
     if return_per_dim:
         return (Dav, Davsd), (D, Dsd)
 
