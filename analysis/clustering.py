a85d8df03c84abc5bd7d634a54be0be9caba301e
Michael King <michael.king@uni-konstanz.de>
2019-06-28 11:26:36 +0200

    add calculate_cluster_around_group

--- analysis/clustering.py
+++ analysis/clustering.py
@@ -49,6 +49,63 @@ def calculate_cluster_per_atom(ag, cutoff, backend='serial'):
     return clusterID
 
 
+def calculate_cluster_around_group(ag_cluster, ag, cutoff, cutoff_cluster=None, backend='serial'):
+    """
+    Distance based clustering around a initial atom group
+
+    Parameters
+    ----------
+    ag_cluster : MDAnalysis.core.groups.AtomGroup
+        Atom group of the initial cluster
+    ag : MDAnalysis.core.groups.AtomGroup
+        Atom group to cluster
+    cutoff : float
+        Cutoff distance for the clustering of atoms in ``ag``.
+    cutoff_cluster : float or None, optional
+        Cutoff distance for the clustering of atoms in ``ag`` with atoms in ``ag_cluster``.
+        If it's ``None``, ``cutoff`` will be used. Default is ``None``.
+    backend : str, optional
+        Which backend to use for the distance calculation.
+        Note: ``serial`` may be faster then ``OpenMP`` for smaller systems.
+        Default is ``serial``.
+
+    Returns
+    -------
+    ag : MDAnalysis.core.groups.AtomGroup
+        Atomgroup with atoms of the initial configuration and clustered ones
+    """
+    if cutoff_cluster is None:
+        cutoff_cluster = cutoff
+
+    n_atoms = ag.n_atoms
+    clusterID = np.zeros((n_atoms,), dtype=np.bool)
+
+    # get connected molecules
+    distances = distance_array(ag_cluster.positions, ag.positions, box=ag.ts.dimensions, backend=backend)
+    ids = np.where(distances < cutoff_cluster)[1]
+    clusterID[ids] = True
+    del distances
+
+    # get molecule - molecules clusters
+    distances = self_distance_array(ag.positions, box=ag.ts.dimensions, backend=backend)
+    ids = np.where(distances < cutoff)[0]
+
+    mi, mj = np.array(np.triu_indices(n_atoms,k=1))[:,ids]
+
+    while True:
+        done = True
+        mask = np.where(clusterID[mi] != clusterID[mj])[0]
+        if mask.size > 0:
+            i, j = mi[mask], mj[mask]
+            clusterID[i] = clusterID[j] = np.max(np.asarray([clusterID[i], clusterID[j]]), axis=0)
+            done = False
+        if done:
+            break
+
+    ag_new = ag_cluster.union(ag.atoms[clusterID])
+    return ag_new
+
+
 def _calculate_cluster_per_atom_lammps(ag, cutoff):
     """
     Distance based clustering.
