c27f7620fdf6c95abbb6d79c27f03b492b8deb79
Michael King <michael.king@uni-konstanz.de>
2019-10-17 10:10:11 +0200

    update rdf

--- analysis/rdf.py
+++ analysis/rdf.py
@@ -1,5 +1,5 @@
 import numpy as np
-from MDAnalysis.lib.distances import self_distance_array
+from MDAnalysis.lib.distances import self_distance_array, distance_array
 from progress_reporter import ProgressReporter_
 
 
@@ -31,16 +31,17 @@ def human_readable_to_bytes(size):
     return bytes ,size +'B'
 
 
-def calculate_RDF(ag,
-                  range=(0, 15),
-                  bins=100,
-                  start=0,
-                  end=None,
-                  step=1,
-                  backend='serial',  # type: str {'serial', 'OpenMP'}
-                  max_memory_usage=None,
-                  verbose=False
-                  ):
+
+def calculate_rdf_intra(ag,
+                        bin_range=(0, 15),
+                        bins=100,
+                        start=0,
+                        end=None,
+                        step=None,
+                        backend='serial',
+                        max_memory_usage=None,
+                        verbose=False
+                        ):
     """
     Calculates the radial distribution function for all atoms in the AtomGroup with them self.
 
@@ -48,7 +49,7 @@ def calculate_RDF(ag,
     ---------
     ag : MDAnalysis.core.groups.AtomGroup
         Atom group
-    range : tuple(int,int), optional
+    bin_range : tuple(int,int), optional
         Bin range to use. Default is `(0, 15)`.
     bins : int, optional
         Number of bins used. Default is `100`.
@@ -64,6 +65,8 @@ def calculate_RDF(ag,
         Maximum memory to use.
         If it's not `None`, results will be buffered before calculating the histogram.
         Default is `None`.
+    verbose : bool, optional
+        Turns on verbosity
 
     Returns
     -------
@@ -74,16 +77,16 @@ def calculate_RDF(ag,
 
     Examples
     --------
-    >>> bins, rdf = calculate_RDF(ag, bins=100, range=(0, 15),
-                                  start=0, end=1000,
-                                  backend='OpenMP',
-                                  max_memory_usage=2 * 1024 * 1024 * 1024, # 2 GB
-                                  verbose=True)
+    >>> bins, rdf = calculate_rdf_intra(ag, bins=100, range=(0, 15),
+                                        start=0, end=1000,
+                                        backend='OpenMP',
+                                        max_memory_usage=2 * 1024**3, # 2 GB
+                                        verbose=True)
     """
 
     # settings
     rdf_settings = dict(bins=bins,
-                        range=range)
+                        range=bin_range)
 
     # general constants
     n_frames = len(ag.universe.trajectory[start:end:step])
@@ -101,15 +104,11 @@ def calculate_RDF(ag,
 
     # init storage array
     dummy_storage = np.empty((n_buffer, n_pairs), dtype=np.float64)
-
     # init histogram
     count, edges = np.histogram([-1], **rdf_settings)
+    volume = 0  # initialize the volume
+    b = 0       # initialize buffer
 
-    # initialze the volume
-    volume = 0
-
-    # initialize buffer
-    b = 0
     if verbose:
         p = ProgressReporter_()
         p.register(n_frames, description="calculate RDF")
@@ -144,6 +143,125 @@ def calculate_RDF(ag,
     bins = (edges[:-1] + edges[1:]) / 2.0
 
     return bins, rdf
+
+
+def calculate_rdf_inter(ag1,
+                        ag2,
+                        bin_range=(0, 15),
+                        bins=100,
+                        start=0,
+                        end=None,
+                        step=None,
+                        backend='serial',  # type: str, {'serial', 'OpenMP'}
+                        max_memory_usage=None,
+                        verbose=False
+                        ):
+    """
+    Calculates the radial distribution function for all atoms between two AtomGroups.
+
+    Parameter
+    ---------
+    ag1 : MDAnalysis.core.groups.AtomGroup
+        Atom group
+    ag2 : MDAnalysis.core.groups.AtomGroup
+        Atom group
+    bin_range : tuple(int,int), optional
+        Bin range to use. Default is `(0, 15)`.
+    bins : int, optional
+        Number of bins used. Default is `100`.
+    start : int, optional
+        Starting frame. Default is `0`.
+    end : int or None, optional
+        Final frame. `None` for last frame. Default is `None`.
+    step : int, optional
+        Step size. Default is `1`.
+    backend : str, optional
+        Backend to use.  `{'serial', 'OpenMP'}`. Default is `serial`.
+    max_memory_usage : int or None, optional
+        Maximum memory to use.
+        If it's not `None`, results will be buffered before calculating the histogram.
+        Default is `None`.
+    verbose : bool, optional
+        Turns on verbosity
+
+    Returns
+    -------
+    bins : numpy.ndarray
+        Array of the bin centers.
+    rdf : numpy.ndarray
+        Radial distribution function.
+
+    Examples
+    --------
+    >>> bins, rdf = calculate_rdf_inter(ag1, ag2, bins=100, range=(0, 15),
+                                        start=0, end=1000,
+                                        backend='OpenMP',
+                                        max_memory_usage=2 * 1024**3, # 2 GB
+                                        verbose=True)
+    """
+    # settings
+    rdf_settings = dict(bins=bins,
+                        range=bin_range)
+
+    assert ag1.universe == ag2.universe, 'Universe of ag1 and ag2 are different'
+
+    # general constants
+    trj_slice = slice(start, end, step)
+    n_frames = len(ag1.universe.trajectory[trj_slice])
+    n_pairs = ag1.n_atoms * ag2.n_atoms
+
+    # handle maximum memory requirement
+    if max_memory_usage is not None:
+        # calculate the needed array size
+        size_distance_array = int(n_pairs * 8)
+        n_buffer = max_memory_usage // size_distance_array
+        assert n_buffer != 0, "Not enough memory to calculate the rdf with this implementation. " + \
+                              "Need at least {} bytes".format(size_distance_array)
+    else:
+        n_buffer = 1
+
+    # init storage array
+    dummy_storage = np.empty((n_buffer, ag1.n_atoms, ag2.n_atoms), dtype=np.float64)
+    # init histogram
+    count, edges = np.histogram([-1], **rdf_settings)
+    volume = 0  # initialize the volume
+    b = 0       # initialize buffer
+
+    if verbose:
+        p = ProgressReporter_()
+        p.register(n_frames, description="calculate g(r)")
+    for ts in ag1.universe.trajectory[trj_slice]:
+        if verbose:
+            p.update(1)
+        # calculate distances
+        distance_array(ag1.positions, ag2.positions, box=ts.dimensions,
+                       result=dummy_storage[b], backend=backend)
+        b += 1  # go to the next buffer
+        if b == n_buffer:
+            tmp_count, _ = np.histogram(dummy_storage, **rdf_settings)
+            count += tmp_count
+            b = 0  # reset b
+
+        volume += ts.volume
+    if b > 0:
+        tmp_count, _ = np.histogram(dummy_storage[:b], **rdf_settings)
+        count += tmp_count
+    if verbose:
+        p.finish()
+
+    # Volume in each radial shell
+    vol = np.power(edges[1:], 3) - np.power(edges[:-1], 3)
+    vol *= 4 / 3.0 * np.pi
+
+    # Average number density
+    box_vol = volume / n_frames
+    density = n_pairs / box_vol
+
+    rdf = count / (density * vol * n_frames)
+    bins = (edges[:-1] + edges[1:]) / 2.0
+
+    return bins, rdf
+
 
 
 def calculate_S(r, gr, q=None,  rho=1.0):
