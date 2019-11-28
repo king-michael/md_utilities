e2df32941cb6aac5dff3cf1cd53794b9ad06999a
Michael King <michael.king@uni-konstanz.de>
2019-11-28 15:27:30 +0100

    extract more data with ReaderPolymorphic

--- lammps_utils/fileparser/pair_polymorphic.py
+++ lammps_utils/fileparser/pair_polymorphic.py
@@ -1,90 +1,110 @@
 import numpy as np
 
-class ReaderPolymorphic():
+
+class ReaderPolymorphic(object):
     def __init__(self, filename):
         self.filename = filename
         self.read(filename)
+
     def read(self, filename):
         with open(filename, 'r') as fp:
             while True:
                 line = fp.readline().strip()
-                if line[0] == '#': continue
-                else: break
+                if line[0] == '#':
+                    continue
+                else:
+                    break
             # types / self.eta
-            self.ntypes , self.eta = map(int,line.split()) # pair_polymorphic.cpp, line 586 / 591
-            self.npairs = self.ntypes*(self.ntypes+1)//2 # pair_polymorphic.cpp, line 637
-            self.ntriple = self.ntypes * self.ntypes * self.ntypes # pair_polymorphic.cpp, line 638
+            self.ntypes, self.eta = map(int, line.split())  # pair_polymorphic.cpp, line 586 / 591
+            self.npairs = self.ntypes * (self.ntypes + 1) // 2  # pair_polymorphic.cpp, line 637
+            self.ntriple = self.ntypes * self.ntypes * self.ntypes  # pair_polymorphic.cpp, line 638
             assert self.eta == 0, "self.eta has to be 0 for sw"
             # tpye section
-            self.list_types=[]
-            for i in range(self.ntypes): # pair_polymorphic.cpp, line 647
+            self.types = []
+            self.masses = []
+            self.atomic_number = []
+            for i in range(self.ntypes):  # pair_polymorphic.cpp, line 647
                 line = fp.readline().strip()
-                self.list_types.append(())
-            # bins
-            self.nr,self.ng,self.nx,self.maxX  = map(int,map(float, fp.readline().split()))  # pair_polymorphic.cpp, line 623 - 631
-            # Cutoff section
-            self.list_cutoff= []
-            for i in range(self.npairs): # pair_polymorphic.cpp, line 647
-                self.list_cutoff.append([float(i) for i in fp.readline().split()])
+                self.atomic_number.append(line.split()[0])
+                self.masses.append(line.split()[1])
+                self.types.append(line.split()[2])
 
+            self.pairs = [(t, t) for t in self.types]
+            self.pairs.extend([(t1, t2) for i, t1 in enumerate(self.types)
+                               for j, t2 in enumerate(self.types)
+                               if i < j])
+            self.triples = [(t1, t2, t3) for t1 in self.types
+                            for t2 in self.types
+                            for t3 in self.types]
+
+            # bins
+            self.nr, self.ng, self.nx, self.maxX = map(int, map(float,
+                                                                fp.readline().split()))  # pair_polymorphic.cpp, line 623 - 631
+            # Cutoff section
+            self.cutoffs = []
+            self.xi = []
+            for i in range(self.npairs):  # pair_polymorphic.cpp, line 647
+                cut, xi = map(float, fp.readline().split())
+                self.cutoffs.append(cut)
+                self.xi.append(xi)
             self.cutmax = 0
             for i in range(self.npairs):
-                cut = self.list_cutoff[i][0]
+                cut = self.cutoffs[i]
                 if cut > self.cutmax: self.cutmax = cut
 
-            lines = fp.read() # read the rest
+            lines = fp.read()  # read the rest
             lines = [float(i) for i in lines.split()]
 
             offset = 0
             # U
             tmp = []
-            range_U=[]
+            range_U = []
             for i in range(self.npairs):  # pair_polymorphic.cpp, line 671
-                tmp.append(lines[offset:offset+self.nr]) # pair_polymorphic.cpp, line 675
-                offset+=self.nr
-                range_U.append([0,self.list_cutoff[i][0]])
-            self.U=np.array(tmp)
+                tmp.append(lines[offset:offset + self.nr])  # pair_polymorphic.cpp, line 675
+                offset += self.nr
+                range_U.append([0, self.cutoffs[i]])
+            self.U = np.array(tmp)
 
             # V
             tmp = []
-            range_V=[]
+            range_V = []
             for i in range(self.npairs):  # pair_polymorphic.cpp, line 681
-                tmp.append(lines[offset:offset+self.nr]) # pair_polymorphic.cpp, line 684
-                offset+=self.nr
-                range_V.append([0,self.list_cutoff[i][0]])
-            self.V=np.array(tmp)
+                tmp.append(lines[offset:offset + self.nr])  # pair_polymorphic.cpp, line 684
+                offset += self.nr
+                range_V.append([0, self.cutoffs[i]])
+            self.V = np.array(tmp)
             # W
             tmp = []
-            range_W=[]
+            range_W = []
             for i in range(self.npairs):  # pair_polymorphic.cpp, line 690
-                tmp.append(lines[offset:offset+self.nr]) # pair_polymorphic.cpp, line 693
-                offset+=self.nr
-                range_W.append([0,self.list_cutoff[i][0]])
-            self.W=np.array(tmp)
+                tmp.append(lines[offset:offset + self.nr])  # pair_polymorphic.cpp, line 693
+                offset += self.nr
+                range_W.append([0, self.cutoffs[i]])
+            self.W = np.array(tmp)
             # P
             tmp = []
-            range_P=[]
+            range_P = []
             for i in range(self.npairs):  # pair_polymorphic.cpp, line 699
-                tmp.append(lines[offset:offset+self.nr]) # pair_polymorphic.cpp, line 702
-                offset+=self.nr
-                range_P.append([-self.cutmax,self.cutmax])
-            self.P=np.array(tmp)
+                tmp.append(lines[offset:offset + self.nr])  # pair_polymorphic.cpp, line 702
+                offset += self.nr
+                range_P.append([-self.cutmax, self.cutmax])
+            self.P = np.array(tmp)
             # G
             tmp = []
-            range_G=[]
+            range_G = []
             for i in range(self.ntriple):  # pair_polymorphic.cpp, line 710
-                tmp.append(lines[offset:offset+self.ng]) # pair_polymorphic.cpp, line 713
-                offset+=self.ng
-                range_G.append([-1,1])
-            self.G=np.array(tmp)
+                tmp.append(lines[offset:offset + self.ng])  # pair_polymorphic.cpp, line 713
+                offset += self.ng
+                range_G.append([-1, 1])
+            self.G = np.array(tmp)
             # F
             tmp = []
-            range_F=[]
+            range_F = []
             for i in range(self.npairs):  # pair_polymorphic.cpp, line 721
-                tmp.append(lines[offset:offset+self.nx]) # pair_polymorphic.cpp, line 724
-                offset+=self.nx
-                range_F.append([0,self.maxX])
-            self.F=np.array(tmp)
+                tmp.append(lines[offset:offset + self.nx])  # pair_polymorphic.cpp, line 724
+                offset += self.nx
+                range_F.append([0, self.maxX])
+            self.F = np.array(tmp)
 
 
 class WriterPolymorphic():
